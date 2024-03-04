7fe1a204e3709bd9c792cc55114473f754783515
Faranaz08 <faranazannigeri3142@gmail.com>
2024-03-04 19:04:02 +0530

    Store the chat to database

--- app.js
+++ app.js
@@ -4,12 +4,25 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const userRouter = require("./routes/user");
 const sequelize = require("./utils/database");
+const chatRouter = require("./routes/chat");
+const User = require("./models/user");
+const Chat = require("./models/chat");
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://127.0.0.1:5500",
+    credentials: true,
+  })
+);
 
 app.use("/user", userRouter);
+app.use("/chat", chatRouter);
+
+// relations (associations)
+User.hasMany(Chat);
+Chat.belongsTo(User);
 
 sequelize
   .sync()
